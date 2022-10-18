662850c7a5505983c41e640ee530d3c434d2fedc
Jaroslaw Kalicinski <jak0024@arribatec.com>
2022-10-18 11:06:02 +0200

    62ms

---
+++ 800/Word/word.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	input := ""
+	fmt.Scanln(&input)
+	runes := []rune(input)
+
+	if len(runes) == 1 {
+		fmt.Print(input)
+		return
+	}
+
+	minUpper := math.Ceil(float64(len(runes)) / 2.0)
+
+	if len(runes)%2 == 0 {
+		minUpper++
+	}
+
+	upperCount := 0
+
+	for i := 0; i < len(runes); i++ {
+		if unicode.IsUpper(runes[i]) {
+			upperCount++
+		}
+
+		if upperCount >= int(minUpper) {
+			fmt.Print(strings.ToUpper(input))
+			return
+		}
+	}
+
+	fmt.Print(strings.ToLower(input))
+}
