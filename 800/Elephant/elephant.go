6126456e559e3ce060371cd7ec26b43a8c4d39ee
Jaroslaw Kalicinski <jak0024@arribatec.com>
2022-10-18 10:15:58 +0200

    continue

--- 800/Elephant/elephant.go
+++ 800/Elephant/elephant.go
@@ -16,15 +16,19 @@ func main() {
 			if left >= 5 {
 				left = left - 5
 				steps = steps + 1
+				continue
 			} else if left >= 4 {
 				left = left - 4
 				steps = steps + 1
+				continue
 			} else if left >= 3 {
 				left = left - 3
 				steps = steps + 1
+				continue
 			} else if left >= 2 {
 				left = left - 2
 				steps = steps + 1
+				continue
 			} else if left >= 1 {
 				left = left - 1
 				steps = steps + 1
