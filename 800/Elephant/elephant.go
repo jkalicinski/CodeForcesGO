98a64c9a474e03d25194049d84b8b4aeacf5b7e1
Jaroslaw Kalicinski <jak0024@arribatec.com>
2022-10-18 10:05:17 +0200

    15ms

--- 800/Elephant/elephant.go
+++ 800/Elephant/elephant.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	dest := 1
+	fmt.Scanln(&dest)
+	steps := 0
+	left := dest
+
+	for {
+		if left <= 5 {
+			steps = steps + 1
+			break
+		} else {
+			if left >= 5 {
+				left = left - 5
+				steps = steps + 1
+			} else if left >= 4 {
+				left = left - 4
+				steps = steps + 1
+			} else if left >= 3 {
+				left = left - 3
+				steps = steps + 1
+			} else if left >= 2 {
+				left = left - 2
+				steps = steps + 1
+			} else if left >= 1 {
+				left = left - 1
+				steps = steps + 1
+			}
+		}
+	}
+
+	fmt.Print(steps)
 }
